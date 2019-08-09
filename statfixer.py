383342863ca4149289dc6e136288b76705f8a1a7
Yossi Rafelson <yossi.rafelson@gmail.com>
2019-08-08 23:24:07 -0700

    move all this chromedriver junk to its own external module
    find it here https://github.com/Yossi/webdriver

--- statfixer.py
+++ statfixer.py
@@ -5,15 +5,9 @@
 # of next week's stat pull. Adds 1 ap because otherwise agent-stats won't take it.
 
 from time import sleep
-from selenium import webdriver
+from webdriver import get_driver # pip install git+https://github.com/Yossi/webdriver#webdriver
 import logging
 import datetime
-import pickle
-import os
-import requests
-import platform
-from zipfile import ZipFile
-from io import BytesIO
 
 try:
     input = raw_input
@@ -31,48 +25,9 @@ try:
 except:
     HEADLESS = False
 
-options = webdriver.ChromeOptions()
-options.add_argument('log-level=3')
-options.add_argument("user-data-dir=profile/")
-if HEADLESS:
-    options.add_argument('headless')
-    options.add_argument('disable-gpu')
-
-def update_chromedriver(version=''):
-    if not version:
-        version = requests.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE').text.strip()
-    arch = {'Linux': 'linux64', 'Darwin': 'mac64', 'Windows': 'win32'}[platform.system()] # these are the only options available
-    logging.info('%s detected', arch)
-    url = 'https://chromedriver.storage.googleapis.com/{}/chromedriver_{}.zip'.format(version, arch)
-    logging.info('downloading...')
-    zip_file = ZipFile(BytesIO(requests.get(url).content))
-    for name in zip_file.namelist():
-        if name.startswith('chromedriver'):
-            logging.info('unpacking...')
-            with open(name, 'wb') as out:
-                out.write(zip_file.read(name))
-            logging.info('ready')
-            break
-
 def main():
-    # assume chrome is installed. anything else is out of scope
-    # http://chromedriver.chromium.org/downloads/version-selection
-    if not (os.path.isfile('chromedriver') or os.path.isfile('chromedriver.exe')): # bootstrap self
-        logging.info('chromedriver not found')
-        update_chromedriver() # just get newest, dont worry about automatically matching chrome yet
-    
     try:
-        driver = None
-        driver = webdriver.Chrome('./chromedriver', chrome_options=options) # assumes our version of chromedriver works with our version of chrome
-        chrome_version = driver.capabilities[({'version', 'browserVersion'} & driver.capabilities.keys()).pop()] #1
-        CD_version = driver.capabilities['chrome']['chromedriverVersion'].split()[0]
-        version_check_url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{}'.format(chrome_version.rpartition('.')[0]) #2
-        matching_CD_version = requests.get(version_check_url).text.strip() #3
-        if CD_version != matching_CD_version: #5
-            driver.quit() # need to release the file lock
-            logging.info('have chromedriver {}. attempting to update to {}'.format(CD_version, matching_CD_version))
-            update_chromedriver(matching_CD_version) #4
-            driver = webdriver.Chrome('./chromedriver', chrome_options=options) # reopen with fresh new chromedriver
+        driver = get_driver(HEADLESS)
 
         driver.implicitly_wait(5)
         driver.set_window_size(1024, 768)
