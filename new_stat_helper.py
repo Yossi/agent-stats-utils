63e38c4b85cdaec06461e9a8a3aaab8c0ff7f6f2
Yossi Rafelson <yossi.rafelson@gmail.com>
2019-08-15 19:40:55 -0700

    improve helpers

--- new_stat_helper.py
+++ new_stat_helper.py
@@ -1,25 +1,27 @@
 def create_code(new_stats):
-    templates = ["  `{statname}` bigint(20) unsigned DEFAULT NULL,\n",
-                 "  `stats`.`{statname}`,\n",
-                 ", {statnodash}",
-                 "        self.{statnodash} = row.{statnodash}\n",
-                 "        self.{statnodash} = row['{statname}']\n",
-                 "                     {statbacktick}='{{{statnodash}}}',\n",
-                 "                                         {statbacktick}='{{{statnodash}}}',\n",
-                 "                   '{statname}': '({description})',\n",
-                 "                  '{statname}': [{tiers}],\n",
-                 "               '{statnodash}',\n",
-                 ", {statname}",
-                 "{{{{ high_scores(chart{stattemplate}) -}}}}\n"]
-    for template in templates:
+    templates = {
+        'schema.sql > create table': "  `{statname}` bigint(20) unsigned DEFAULT NULL,\n",
+        'schema.sql > stored procedure': "  `stats`.`{statname}`,\n",
+        'Stat.py > fields': ", {statnodash}",
+        'Stat.py > db_load()': "        self.{statnodash} = row.{statnodash}\n",
+        'Stat.py > table_load()': "        self.{statnodash} = row['{statname}']\n",
+        'Stat.py > save(1)': "                     {statbacktick}='{{{statnodash}}}',\n",
+        'Stat.py > save(2)': "                                         {statbacktick}='{{{statnodash}}}',\n",
+        'agent_stats.py > get_stats() > definitions': "                   '{statname}': '({description})',\n",
+        'agent_stats.py > get_badges() > categories (or 3 tier categories)': "                  '{statname}': [{tiers}],\n",
+        'agent_stats.py > summary() > headers': "               '{statnodash}',\n",
+        'agent_stats.py > summary() > sql_before & sql_after': ", {statname}",
+        'templates': "{{{{ high_scores(chart{stattemplate}) -}}}}\n"}
+    for place, template in templates.items():
         for stat, description, tiers in new_stats:
             stat_dict = {'description': description,
-                         'tiers': ', '.join([tier.strip().replace(',', '') for tier in tiers.split('\t')[1:]]),
+                         'tiers': ', '.join([tier.strip().replace(',', '') for tier in tiers.split('\t')[1:] if tier != 'N/A']),
                          'statname': stat,
                          'statnodash': stat.replace('-', '_'),
                          'statbacktick': f'`{stat}`' if '-' in stat else stat,
                          'stattemplate': f"['{stat}']" if '-' in stat else f'.{stat}',
                         }
+            print(place + ':')
             print(template.format(**stat_dict), end='')
         print('\n')
 
@@ -27,9 +29,9 @@ def create_code(new_stats):
 #https://www.agent-stats.com/faq.php
 #copy the new stat(s) from the table and paste them here as is
 
-a = '''myriad_hack
-Myriad: Unique Portals Hacked
-	100 	350 	750	N/A	N/A'''
+a = '''aurora_glyph
+Aurora: Unique Glyph Hack Points
+	100 	500 	1,000	N/A	N/A'''
 
 it = iter(a.split('\n'))
 
