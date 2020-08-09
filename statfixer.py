33771141b8b0f71908c9dced4a073f28f13b01e9
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-08-09 16:36:11 -0700

    Fix statfixer.py

--- statfixer.py
+++ statfixer.py
@@ -31,32 +31,26 @@ def main():
 
         driver.implicitly_wait(5)
         driver.set_window_size(1024, 768)
-        driver.get('https://www.agent-stats.com/export.php')
+        driver.get('https://www.agent-stats.com/quick_add.php')
         sleep(3)
         logging.info('url loaded')
-        
+
         if 'Sign in' in driver.find_element_by_tag_name('BODY').text:
             if HEADLESS:
                 driver.save_screenshot('selenium.png')
                 # not really how to raise exceptions, but this line crashes and that's the point here
                 raise 'You need to login. Set HEADLESS = False and try again.'
             input('Press enter after you have logged in... ')
-        
-        data = driver.find_elements_by_tag_name("tr")[-1].text # needs more brains than simply "the last one"
-        
-        temp = data.split()
-        now = datetime.datetime.now()
-        temp[0] = now.strftime('%Y-%m-%d')
-        temp[1] = now.strftime('%H:%M:%S')
-        temp[2] = str(int(temp[2])+1)
-        temp[-1] = '"statfixer"'
-        data = ' '.join(temp)
-        logging.info(data)
-        driver.get('https://www.agent-stats.com/import.php')
-        textarea = driver.find_element_by_tag_name('textarea')
-        textarea.click()
-        textarea.send_keys(data)
-        driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/form/input').click()
+
+        driver.get('https://www.agent-stats.com/quick_add.php') # loads up with everything already filled in. basically does exactly what we need
+
+        comment = driver.find_element_by_xpath("//input[@name='comment']")
+        comment.clear()
+        comment.click()
+        comment.send_keys('statfixer')
+
+        save = driver.find_element_by_xpath("//input[@value='save']")
+        save.click()
 
         #driver.save_screenshot('selenium.png')
         #logging.info('screenshot saved')
