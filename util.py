b89dfcaf54943821a4738cb5dc4696c2f6ea3760
Yossi <yossi.rafelson@gmail.com>
2022-05-08 15:36:09 -0700

    Change to use Ouath2 for email to keep working after May 30

--- util.py
+++ util.py
@@ -57,19 +57,49 @@ def exec_mysql(sql, retries=2):
 #######
 
 
-import smtplib
+import oauth2client
+from oauth2client import client, tools, file
+from apiclient import errors, discovery
+import httplib2
 import platform
 from ftfy import fix_text # pip install ftfy
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from secrets import gmail_user, gmail_pwd
+from secrets import gmail_sender
+import base64
+
+def get_credentials():
+    credential_path = 'gmail-python-email-send.json'
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = oauth2client.client.flow_from_clientsecrets('client_secret.json', 'https://www.googleapis.com/auth/gmail.send')
+        flow.user_agent = 'agent-stats-util'
+        credentials = oauth2client.tools.run_flow(flow, store)
+        logging.info('Storing credentials to ' + credential_path)
+    return credentials
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        logging.info('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        logging.error('An error occurred: %s' % error)
+        return "Error"
 
 def mail(to, subject, text, attach=False, host=False):
-    # to needs to be a list
+       # to has to be a list
+
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http, cache_discovery=False)
+
+
     msg = MIMEMultipart()
 
-    msg['From'] = gmail_user
+    msg['From'] = gmail_sender
     msg['To'] = ", ".join(to) if len(to[0])-1 else to
     if host:
         subject = platform.node() + ' ' + subject
@@ -82,12 +112,7 @@ def mail(to, subject, text, attach=False, host=False):
         part['Content-Disposition'] = 'attachment; filename="%s.txt"' % subject
         msg.attach(part)
 
-    mailServer = smtplib.SMTP("smtp.gmail.com", 587)
-    mailServer.ehlo()
-    mailServer.starttls()
-    mailServer.ehlo()
-    mailServer.login(gmail_user, gmail_pwd)
-    mailServer.sendmail(gmail_user, to, msg.as_string())
-    # Should be mailServer.quit(), but that crashes...
-    mailServer.close()
-    logging.info((msg['To'], msg['Subject']))
+    message = {'raw': base64.urlsafe_b64encode(msg.as_bytes()).decode()}
+
+
+    return SendMessageInternal(service, "me", message)
